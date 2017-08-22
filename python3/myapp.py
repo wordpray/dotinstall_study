00daccaeda11e60fb7693c0bada896edb728aeff
y.yoshiki <labiatejoker@gmail.com>
2017-08-22 15:17:51 +0900

    Create myapp.py

---
+++ python3/myapp.py
@@ -0,0 +1,24 @@
+x = 10
+print(x / 3)
+print(x // 3)
+print(x % 3)
+print(x ** 3)
+
+print("----")
+
+y = 4
+y = y + 12
+y += 12
+
+print y
+
+print("----")
+
+print(True and False)
+print(True or False)
+print(not False)
+
+print("----")
+
+print("Hello" + "world")
+print("Hello" * 3)
