ebb552be8e598843d4ac48ff9a90c7f9d8d21266
y.yoshiki <labiatejoker@gmail.com>
2017-09-04 16:46:16 +0900

    Finish study sinatra

---
+++ sinatra/app.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+require 'rack/csrf'
+
+use Rack::Session::Cookie, secret: "thissomethingsecret"
+use Rack::Csrf, raise: true
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: './bbs.db'
+  )
+
+class Comment < ActiveRecord::Base
+  validates :body, presence: true
+end
+
+get '/' do
+  @title = "My site"
+  @comments= Comment.all
+  erb :index
+end
+
+post '/create' do
+  Comment.create(body: params[:body])
+  redirect to('/')
+end
+
+post '/destroy' do
+  Comment.find(params[:id]).destroy
+end
