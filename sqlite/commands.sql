af3ef787523f52629cac472e608b3763e13befb6
y.yoshiki <labiatejoker@gmail.com>
2017-09-08 17:56:34 +0900

    Study SQlite

--- sqlite/commands.sql
+++ sqlite/commands.sql
@@ -8,11 +8,12 @@ CREATE table posts(
   body text
 );
 
--- list tables
-alter table posts rename to articles;
+insert into posts(title, body) values('title1', 'body1');
+insert into posts(id, title, body) values(null, 'title2', 'body2');
+insert into posts(title, body) values('title3', 'body3');
+insert into posts(title, body) values('title4', 'it''s body4');
+insert into posts(title, body) values('title5', 'bo
+  d
+  y5');
 
-.tables
-
-alter table articles add column email text;
-
-.schema
+select * from posts;
