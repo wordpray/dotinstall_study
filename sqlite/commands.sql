11e3e13c9754e01c19d636bae3ee01f81e64bba2
y.yoshiki <labiatejoker@gmail.com>
2017-09-08 17:52:22 +0900

    Study SQlite

--- sqlite/commands.sql
+++ sqlite/commands.sql
@@ -1,12 +1,18 @@
 -- table
 
-CREATE table if not exists posts(
-  id,
-  title,
-  body
+drop table if exists posts;
+
+CREATE table posts(
+  id integer primary key,
+  title text,
+  body text
 );
 
 -- list tables
+alter table posts rename to articles;
+
 .tables
+
+alter table articles add column email text;
 
 .schema
