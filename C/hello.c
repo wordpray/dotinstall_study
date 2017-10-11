b274371725119778852d7301d2b1f35f7fa2f545
y.yoshiki <labiatejoker@gmail.com>
2017-10-11 15:07:38 +0900

    Study s

--- C/hello.c
+++ C/hello.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-void f(void){
-  static int a = 0;
-  a ++;
-  printf("a: %d\n", a);
-}
-
 int main(void){
-  f();
-  f();
-  f();
-  f();
+  int sales[3];
 
+  sales[0] = 200;
+  sales[1] = 100;
+  sales[2] = 400;
+
+  int sales2[] = {200, 40, 300};
+
+  printf("%d\n", sales2[1]);
   return 0;
 }
